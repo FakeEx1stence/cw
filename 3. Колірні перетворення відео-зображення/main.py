49f633935b0bb733c058183fec8e662a77974832
FakeEx1stence <144431638+FakeEx1stence@users.noreply.github.com>
2025-04-23 01:55:33 +0300

    fixed 23.04

--- 3. Колірні перетворення відео-зображення/main.py
+++ 3. Колірні перетворення відео-зображення/main.py
@@ -1,28 +1,26 @@
-import cv2                                  # Імпорт бібліотеки CV2
+import cv2
+import numpy as np
 
 def main():
-    img = cv2.imread('C:\\Users\\POTUZHNYY\\Desktop\\1.jpg', cv2.IMREAD_COLOR) # Функція читання зображення
+    cap = cv2.VideoCapture(0)
 
-    if img is None:                                         # Перевірка на існування зображення за шляхом вище
-        print("Error: Could not read the image file.")
+    if not cap.isOpened():  # Перевірка на існування потоку відеозображення
+        print("Error: Could not open the camera.")
         return
 
-    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)              # Переведення зображення з BGR(звичайний кольоровий простір cv2) у RGB
-    xyz = cv2.cvtColor(rgb, cv2.COLOR_RGB2XYZ)              # Переведення зображення з RGB у XYZ
-    hsv = cv2.cvtColor(rgb, cv2.COLOR_RGB2HSV)              # Переведення зображення з RGB у HSV
-    gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)             # Переведення зображення з RGB у GRAY
-    yuv = cv2.cvtColor(rgb, cv2.COLOR_RGB2YUV)              # Переведення зображення з RGB у YUV
+    while True:
 
-    cv2.imshow('Original', img)  # Виведення звичайного зображення на екран
-    cv2.imshow('RGB', rgb)  # Виведення зображення у RGB-просторі на екран
-    cv2.imshow('HSV', hsv)  # Виведення зображення у HSV-просторі на екран
-    cv2.imshow('XYZ', xyz)  # Виведення зображення у XYZ-просторі на екран
-    cv2.imshow('GRAY', gray)  # Виведення зображення у GRAY-просторі на екран
-    cv2.imshow('YUV', yuv)  # Виведення зображення у YUV-просторі на екран
+        ret, frame = cap.read()  # Читання кадру відео
 
-    cv2.waitKey(0)                                      # Функція очікування дій користувача(Задля того, щоб зображення не закрилося миттєво)
-    cv2.destroyAllWindows()                            # Функція закривання усіх вікон
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)             # Переведення зображення у GRAY-простір
+        cv2.imshow('OriginalFrame', frame)  # Вивід кадру відео на монітор
+        cv2.imshow('GrayColoredFrame', gray_frame)  # Вивід кадру відео на монітор з кольоровим простором GRAY
 
+        if cv2.waitKey(1) & 0xFF == ord('q'):  # Чекаємо натискання кнопки q
+            break
 
-if __name__ == '__main__':                     # Викликання функції main
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
     main()
